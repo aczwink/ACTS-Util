0d811c2c3560540b0ca30115786d3bd6e0eb33b1
Amir Czwink <amir130@hotmail.de>
2026-01-31 17:54:45 +0100

    fix

--- test/src/Expector.ts
+++ test/src/Expector.ts
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * */
-import { EqualsAny } from "acts-util-core";
+import { EqualsAny } from "@aczwink/acts-util-core";
 
 export class Expector<T>
 {

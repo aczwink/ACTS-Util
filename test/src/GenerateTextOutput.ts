159a44f1858126566739d0271e67581894b5c475
Amir Czwink <amir130@hotmail.de>
2026-04-11 10:48:41 +0200

    added colors to test output

--- test/src/GenerateTextOutput.ts
+++ test/src/GenerateTextOutput.ts
@@ -1,6 +1,6 @@
 /**
  * ACTS-Util
- * Copyright (C) 2024 Amir Czwink (amir130@hotmail.de)
+ * Copyright (C) 2024-2026 Amir Czwink (amir130@hotmail.de)
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -15,12 +15,34 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * */
-
+import chalk from 'chalk';
 import { TestRunResult } from "./Definitions";
+
+function DecorateWithColor(text: string, decoration: "green" | "red-bold")
+{
+    if(process.stdout.isTTY)
+    {
+        switch(decoration)
+        {
+            case "green":
+                return chalk.green(text);
+            case "red-bold":
+                return chalk.red.bold(text);
+        }
+    }
+    return text;
+}
 
 function ErrorToString(error: Error)
 {
     return error.message;
+}
+
+function TestResultToString(result: TestRunResult)
+{
+    if(result.error === undefined)
+        return DecorateWithColor("success", "green");
+    return DecorateWithColor("failed", "red-bold") + " (" + ErrorToString(result.error) + ")";
 }
 
 export function GenerateTextOutput(results: TestRunResult[])
@@ -28,7 +50,7 @@ export function GenerateTextOutput(results: TestRunResult[])
     const data = [];
     for (const result of results)
     {
-        const text = "Result of test case '" + result.testSuite + "/" + result.testTitle + "': " + (result.error === undefined ? "success" : "failed (" + ErrorToString(result.error) + ")" + " file: " + result.filePath);
+        const text = "Result of test case '" + result.testSuite + "/" + result.testTitle + "': " + TestResultToString(result) + " file: " + result.filePath;
         data.push(text);
     }
 
